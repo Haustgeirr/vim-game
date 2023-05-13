d2a78d20be28a0e6a1db2186e902123a93eb5789
Haustgeirr <3540179+Haustgeirr@users.noreply.github.com>
2023-05-13 18:33:20 +0100

    added grid

--- src/components/GameCanvas/GameCanvas.tsx
+++ src/components/GameCanvas/GameCanvas.tsx
@@ -1,28 +1,56 @@
-import React from "react";
-import type P5 from "p5";
-import dynamic from "next/dynamic";
+import React from 'react';
+import type P5 from 'p5';
+import dynamic from 'next/dynamic';
 
 // import `react-p5` on client-side
-const Sketch = dynamic(() => import("react-p5").then((mod) => mod.default), {
+const Sketch = dynamic(() => import('react-p5').then((mod) => mod.default), {
   ssr: false,
 });
 
-type CanvasProps = {
-  // Your component props
-};
+const CELL_SIZE = 32;
 
-let x = 50;
-const y = 50;
+function calcGrid(windowWidth: number, windowHeight: number, cellSize: number) {
+  function calcCellsInDimension(dimension: number) {
+    const cells = Math.floor(dimension / cellSize);
+    const rem = dimension % cellSize;
 
-const GameCanvas = (props: CanvasProps) => {
+    return rem === 0 ? cells - 1 : cells;
+  }
+
+  const cellsX = calcCellsInDimension(windowWidth);
+  const cellsY = calcCellsInDimension(windowHeight);
+
+  return {
+    cellsX,
+    cellsY,
+    width: cellsX * cellSize,
+    height: cellsY * cellSize,
+  };
+}
+
+const GameCanvas = () => {
+  if (typeof window === 'undefined') {
+    return null;
+  }
+
+  const { cellsX, cellsY, width, height } = calcGrid(window.innerWidth, window.innerHeight, CELL_SIZE);
+
   const setup = (p5: P5, canvasParentRef: Element) => {
-    p5.createCanvas(500, 500).parent(canvasParentRef);
+    p5.createCanvas(width, height).parent(canvasParentRef);
   };
 
   const draw = (p5: P5) => {
-    p5.background(0);
-    p5.ellipse(x, y, 70, 70);
-    x++;
+    p5.background(50);
+    p5.stroke(p5.color(100));
+    p5.strokeWeight(1);
+    for (let x = 0; x < cellsX; x++) {
+      for (let y = 0; y < cellsY; y++) {
+        p5.noFill();
+        p5.rect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+      }
+    }
+
+    p5.noStroke();
   };
 
   return <Sketch setup={setup} draw={draw} />;
