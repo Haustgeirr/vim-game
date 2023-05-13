81f3f9bea93c610b175fb241e8d9400ad7c6eba8
Haustgeirr <3540179+Haustgeirr@users.noreply.github.com>
2023-05-13 18:32:43 +0100

    updated prettier config

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,6 +1,9 @@
 /** @type {import("prettier").Config} */
 const config = {
   plugins: [require.resolve("prettier-plugin-tailwindcss")],
+  singleQuote: true,
+  trailingComma: "es5",
+  printWidth: 120,
 };
 
 module.exports = config;
